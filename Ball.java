c79ff03d48a200a4a58da5d1f2dcccba39b18261
Tanya von Degurechaff <72034257+Victorthemachine@users.noreply.github.com>
2020-09-30 10:15:21 +0200

    Skeleton of the ball class

---
+++ Ball.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snooker.objects;
+
+import javafx.scene.shape.Circle;
+
+/**
+ *
+ * @author martin
+ */
+public class Ball {
+
+    private Circle bounds;
+    private double[] center;
+    private double radius;
+
+    public Ball(Circle bounds) {
+        this.bounds = bounds;
+        center[0] = bounds.getCenterX();
+        center[1] = bounds.getCenterY();
+        radius = bounds.getRadius();
+    }
+
+    public Ball(double[] center, double radius) {
+        this.center = center;
+        this.radius = radius;
+        bounds = new Circle(center[0], center[1], radius);
+    }
+
+    public Ball(double radius) {
+        this.radius = radius;
+        bounds = new Circle(radius);
+    }
+
+}
