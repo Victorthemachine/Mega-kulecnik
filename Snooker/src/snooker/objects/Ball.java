4b9adde9f058fa98b1aeaa4e6c7a6b55c84faa8a
Tanya von Degurechaff <72034257+Victorthemachine@users.noreply.github.com>
2020-09-30 10:16:48 +0200

    Add files via upload

--- Snooker/src/snooker/objects/Ball.java
+++ Snooker/src/snooker/objects/Ball.java
@@ -5,10 +5,34 @@
  */
 package snooker.objects;
 
+import javafx.scene.shape.Circle;
+
 /**
  *
  * @author martin
  */
 public class Ball {
-    
+
+    private Circle bounds;
+    private double[] center;
+    private double radius;
+
+    public Ball(Circle bounds) {
+        this.bounds = bounds;
+        center[0] = bounds.getCenterX();
+        center[1] = bounds.getCenterY();
+        radius = bounds.getRadius();
+    }
+
+    public Ball(double[] center, double radius) {
+        this.center = center;
+        this.radius = radius;
+        bounds = new Circle(center[0], center[1], radius);
+    }
+
+    public Ball(double radius) {
+        this.radius = radius;
+        bounds = new Circle(radius);
+    }
+
 }
