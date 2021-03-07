5cc5b5de33e82f14d55574b901b87ed8d0f98926
gofop2 <tadeas.bajer@student.gyarab.cz>
2021-03-07 14:28:43 +0100

    Basic css (furt to není gucci)

--- mega-snooker/client/src/pages/Lobby.js
+++ mega-snooker/client/src/pages/Lobby.js
@@ -5,14 +5,14 @@ import './../styles/Menu.css';
 
 export default function Menu() {
     /*    let history = useHistory();
-    
+
         const redirect = () => {
             history.push('/lobby');
         };*/
     /**
-     *            
+     *
              * FIX: adjust this label node to be a slider ON/OFF
-             
+
              <label >
              Multiplayer:
              <input type="checkbox"/>
@@ -21,11 +21,18 @@ export default function Menu() {
      */
 
     return (
+      <div className='container'>
+      <div className='box'>
+      </div>
+      <div className='head'>
+        <h1> Play </h1>
+        </div>
         <div className='col'>
             <button onClick={console.log('Singleplayer button was clicked!')}>Singleplayer</button>
             <button>Against AI</button>
             <button>Multiplayer</button>
             <button>Settings</button>
+        </div>
         </div>
     );
 }
--- mega-snooker/client/src/pages/Menu.js
+++ mega-snooker/client/src/pages/Menu.js
@@ -11,11 +11,23 @@ export default function Menu() {
     };
 
     return (
+      <div className='container'>
+      <div className='box'>
+      </div>
+      <div className='head'>
+        <h1> Mega kulečník </h1>
+      </div>
         <div className='col'>
             <button onClick={console.log('Login button was clicked!')}>Login/Register</button>
             <button onClick={redirect}>Play</button>
             <button>Statistics</button>
             <button>Settings</button>
         </div>
+        <div className='pravidla'>
+          <p1> Co je to kulečník?
+          <br /></p1>
+          <p2> Balh bluh blah blah bluuu UwU asd blu m abla </p2>
+        </div>
+      </div>
     );
 }
